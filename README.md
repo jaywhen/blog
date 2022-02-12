fffd545bbfea0ffd1352655b7f441a651726ed7b
jaywhen <jaywhen@qq.com>
2022-02-12 19:54:50 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# A Notion powered blog
+
+### config
+
+```bash
+// create a .env.local file in the project root
+// in .env.local
+NOTION_KEY = "YOUR NOTION KEY"
+NOTION_DATABASE_ID = "YOUR NOTION DATABASE ID"
+```
+
+### Reference
+
+[spencerwoo.com](https://github.com/spencerwooo/spencerwoo.com)
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import Script from 'next/script';
 import NextNProgress from 'nextjs-progressbar';
 import Layout from '../components/Layout';
 import '../styles/globals.css'
@@ -6,19 +5,6 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.MEASURE_ID}`}
-      />
-      <Script id="ga-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.MEASURE_ID}');
-        `}
-      </Script>
       <NextNProgress height={2} color="rgb(47, 79, 79)" options={{ showSpinner: false }} />
       <Component {...pageProps} />
     </Layout>
