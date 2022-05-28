cd236934defa4b6fb53bc5f819a4d8ce941052f1
jaywhen <jaywhen@qq.com>
2022-05-28 17:39:14 +0800

    remove lock files & foot icons & modified link format

--- components/FontLink.js
+++ components/FontLink.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function FontLink({ text, link }) {
-  return <a href={link} className="underline decoration-2 decoration-slate-800 hover:decoration-pink-700" target="_blank" rel="noopener noreferrer">{text}</a>;
+  return (
+    <a href={link} 
+      className="underline decoration-2 decoration-slate-800 hover:decoration-pink-700" target="_blank" 
+      rel="noopener noreferrer">
+      {text}
+    </a>
+  );
 }
 
 export default FontLink;
