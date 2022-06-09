2ba4847a2a8066d28c421e43d9e99e5de36e1b1b
jaywhen <jaywhen@qq.com>
2022-06-10 00:29:29 +0800

    set [slug].js revalidate as 10

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -78,7 +78,7 @@ export const getStaticProps = async ({ params }) => {
   // const blocksWithChildren = blocksWithPic;
 
   return {
-    props: { page, blocks: blocksWithChildren, revalidate: REBUILD_TIME }
+    props: { page, blocks: blocksWithChildren, revalidate: 10 }
   }
 }
 
